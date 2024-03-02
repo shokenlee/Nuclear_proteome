137d2e0915104702edddc440e732ad370d08f5b9
Shoken Lee <shoken1227@gmail.com>
2024-03-02 12:09:57 -0500

    Taxinomy API added

--- my_config.py
+++ my_config.py
@@ -1,5 +1,6 @@
 # uniprot API URL
 WEBSITE_API = "https://rest.uniprot.org/uniprotkb/search"
+Taxonomy_API = "https://rest.uniprot.org/taxonomy/"
 
 # organism id
 organism_id_list = {'Homo sapiens': '9606', 'Mus musculus': '10090'}
